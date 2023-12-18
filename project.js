de2e030eaec985143ba510c4871e710f940bb546
yaseen <yasikalleri@gmail.com>
2023-12-18 21:25:59 +0530

    spin function created

--- project.js
+++ project.js
@@ -1,4 +1,18 @@
 const prompt= require("prompt-sync")();
+const ROWS =3 ;
+const COLS =3 ;
+const SYMBOL_COUNT = {
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+const SYMBOL_VALUE={
+    "A":5,
+    "B":4,
+    "C":3,
+    "D":2 
+}
 const deposit = ()=>{
     while(true){
       const depositAmount =prompt("Enter a deposit amount :");
@@ -36,8 +50,28 @@ if(isNaN(numberbetAmount)|| numberbetAmount<=0 || numberbetAmount>balance/number
     }
 
 }
+const spin =()=>{
+    const symbols = [];
+    for (const [symbol,count] of Object.entries(SYMBOL_COUNT)){
+       for(let i=0;i<count;i++){
+        symbols.push(symbol)
+       }
+    }
+    
+    const reels = [];
+    for(let i = 0 ; i<COLS;i++){
+        const reelSymbols = [...symbols];
+        reels.push([]);
+        for(let j=0;j<ROWS;j++){
+            const randomIndex = Math.floor(Math.random()*reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex,1);
+        }
+    }
+return reels;
+}
+console.log(spin());
 let balance = deposit()
-console.log(balance);
 const numberOFLines = getNumberOfLines();
-console.log(numberOFLines);
 const betAmount = getBetAmount(balance,numberOFLines)
