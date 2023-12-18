5f620a8c5bc2bec708923e4a5ffd93f0a1fe4e12
yaseen <yasikalleri@gmail.com>
2023-12-18 07:34:46 +0530

    collected lines,bet

--- project.js
+++ project.js
@@ -11,5 +11,33 @@ if(isNaN(numberdepositAmount)|| numberdepositAmount<=0){
     }
 
 }
-const depositAmount = deposit()
-console.log(depositAmount);
+
+const getNumberOfLines = ()=>{
+    while(true){
+      const lines =prompt("Enter the number of lines to bet on (1-3) :");
+const numberOFLines = parseFloat(lines);
+if(isNaN(numberOFLines)|| numberOFLines<=0 || numberOFLines >3){
+    console.log("Invalid number ,try again");
+}else{
+    return numberOFLines;
+}
+    }
+
+}
+const getBetAmount = (balance ,numberOFLines)=>{
+    while(true){
+      const betAmount =prompt("Enter the total bet amount per line :");
+const numberbetAmount = parseFloat(betAmount);
+if(isNaN(numberbetAmount)|| numberbetAmount<=0 || numberbetAmount>balance/numberOFLines){
+    console.log("Invalid Amount ,try again");
+}else{
+    return numberbetAmount;
+}
+    }
+
+}
+let balance = deposit()
+console.log(balance);
+const numberOFLines = getNumberOfLines();
+console.log(numberOFLines);
+const betAmount = getBetAmount(balance,numberOFLines)
